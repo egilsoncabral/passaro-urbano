ee4a6fbd6d157e1039c15776763a90a6f2ab5e88
egilson.cabral <egilson.cabral@dataprev.gov.br>
2018-10-31 17:01:19 -0300

    Implementando paginas de ofertas dinamicas

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,22 +1,32 @@
 import {Oferta} from './shared/oferta.model'
 import { Http } from '@angular/http';
 import {Injectable} from '@angular/core'
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService{
 
+
     constructor(private http:Http){}
 
     public getOfertas():Promise<Oferta[]>{
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}?destaque=true`)
         .toPromise()
         .then((resposta:any) => resposta.json())
     }
 
     public getOfertasPorCategoria(categoria:string): Promise<Oferta[]>{
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
         .toPromise()
         .then((resposta:any) => resposta.json())
 
     }
+
+    public getOfertasPorId(id:number): Promise<Oferta>{
+        return this.http.get(`${URL_API}?id=${id}`)
+        .toPromise()
+        .then((resposta:any) => resposta.json()[0])
+
+    }
+
 }
