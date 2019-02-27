8d9e20d202718a814bcabfeecd715c00cefc0b3e
egilson.cabral <egilson.cabral@dataprev.gov.br>
2019-02-27 09:43:10 -0300

    Criando validadores do form de ordem de compra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+
+  public endereco: string = ''
+  public numero: string = ''
+  public complemento: string = ''
+  public formaPagamento: string = ''
+
+  //Controle
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public atualizaEndereco(endereco:string):void{
+    this.endereco = endereco
+    this.enderecoValido = endereco.length > 3 ? true : false
+  }
+
+  public atualizaNumero(numero:string):void{
+    this.numero = numero
+    this.numeroValido = numero.length > 0 ? true : false
+  }
+
+  public atualizaComplemento(complemento:string):void{
+    this.complemento = complemento
+    this.complementoValido = complemento.length > 0 ? true : false
+  }
+
+  public atualizaFormaPagamento(formaPagamento:string):void{
+    this.formaPagamento = formaPagamento
+    this.formaPagamentoValido = formaPagamento.length > 0 ? true : false
   }
 
 }
